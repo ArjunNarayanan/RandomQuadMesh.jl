05480e57df23e2451ed42ff858265a856416ffb1
ArjunNarayanan <arjun.narayanan91@gmail.com>
2022-09-21 10:05:45 -0700

    fixed module name issue

--- src/random_quad_mesh.jl
+++ src/random_quad_mesh.jl
@@ -1,6 +1,6 @@
 function random_quad_mesh(npts)
-    mesh = RM.random_polygon_trimesh(npts)
-    q, t = RM.match_tri2quad(mesh)
-    quad_mesh = RM.triquad_refine(mesh.p, q, t)
+    mesh = random_polygon_trimesh(npts)
+    q, t = match_tri2quad(mesh)
+    quad_mesh = triquad_refine(mesh.p, q, t)
     return quad_mesh
 end
