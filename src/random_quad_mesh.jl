dab4d74c8de4bb58dc5df492fc87dc3ad747d65f
ArjunNarayanan <arjun.narayanan91@gmail.com>
2022-10-12 11:25:35 -0700

    no major changes

--- src/random_quad_mesh.jl
+++ src/random_quad_mesh.jl
@@ -1,5 +1,11 @@
 function random_quad_mesh(npts)
-    mesh = random_polygon_trimesh(npts)
+    pv = random_polygon(npts)
+    return quad_mesh(pv)
+end
+
+function quad_mesh(boundary_points)
+    p, t = polytrimesh([boundary_points[:,[1:end;1]]], [], Inf, "pQ")
+    mesh = Mesh(p, t)
     q, t = match_tri2quad(mesh)
     quad_mesh = triquad_refine(mesh.p, q, t)
     return quad_mesh
--- test/test_random_mesh.jl
+++ test/test_random_mesh.jl
@@ -1,4 +1,4 @@
-using Revise
+# using Revise
 using Test
 using RandomQuadMesh
 RM = RandomQuadMesh
@@ -7,11 +7,11 @@ using Random
 Random.seed!(123)
 
 
-mesh = RM.random_polygon_trimesh(10)
-q, t = RM.match_tri2quad(mesh)
+poly = RM.random_polygon(10)
+# mesh = RM.random_polygon_trimesh(10)
+# q, t = RM.match_tri2quad(mesh)
+# quad_mesh = RM.triquad_refine(mesh.p, q, t)
 
-quad_mesh = RM.triquad_refine(mesh.p, q, t)
-
-using QuadMeshGame
-QM = QuadMeshGame
-qmesh = QM.QuadMesh(quad_mesh.p, quad_mesh.t, quad_mesh.t2t, quad_mesh.t2n)
+# using QuadMeshGame
+# QM = QuadMeshGame
+# qmesh = QM.QuadMesh(quad_mesh.p, quad_mesh.t, quad_mesh.t2t, quad_mesh.t2n)
